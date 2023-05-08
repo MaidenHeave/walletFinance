91e071fb2af1a48f922724bebed378d9c785bede
Rhino <rhino@localhost.localdomain>
2023-05-08 10:26:57 +0530

    new file for image urls

---
+++ lib/assets/paymentUrls.dart
@@ -0,0 +1,21 @@
+const List<Map<String, dynamic>> paymentOperator = [
+  {
+    "name": "Orange Money",
+    "logoLink":
+        "https://logos-marques.com/wp-content/uploads/2021/07/Orange-Money-logo.png"
+  },
+  {
+    "name": "Moov Money",
+    "logoLink":
+        "https://upload.wikimedia.org/wikipedia/fr/1/1d/Moov_Africa_logo.png"
+  },
+  {
+    "name": "MTN Money",
+    "logoLink": "https://www.sanwi-informatique.com/img/cms/mtn-money.png"
+  },
+  {
+    "name": "Wave Mobile",
+    "logoLink":
+        "https://cdn-website.partechpartners.com/media/images/Wave-logo-website.original.png"
+  }
+];
--- lib/pages/selectWithdraw.dart
+++ lib/pages/selectWithdraw.dart
@@ -2,34 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wallet/pages/withdrawPage.dart';
 
+import 'package:wallet/assets/paymentUrls.dart';
+
 class WithdrawSelector extends StatefulWidget {
+  const WithdrawSelector({Key? key}) : super(key: key);
+
   @override
   WithdrawSelectorState createState() => WithdrawSelectorState();
 }
 
 class WithdrawSelectorState extends State<WithdrawSelector> {
-  final List<Map<String, dynamic>> paymentOperator = [
-    {
-      "name": "Orange Money",
-      "logoLink":
-          "https://logos-marques.com/wp-content/uploads/2021/07/Orange-Money-logo.png"
-    },
-    {
-      "name": "Moov Money",
-      "logoLink":
-          // "https://www.moov-africa.ci/wp-content/uploads/2022/05/icon-assistance-moovci_Plan-de-travail-1-1.png"
-          "https://upload.wikimedia.org/wikipedia/fr/1/1d/Moov_Africa_logo.png"
-    },
-    {
-      "name": "MTN Money",
-      "logoLink": "https://www.sanwi-informatique.com/img/cms/mtn-money.png"
-    },
-    {
-      "name": "Wave Mobile",
-      "logoLink":
-          "https://cdn-website.partechpartners.com/media/images/Wave-logo-website.original.png"
-    }
-  ];
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
